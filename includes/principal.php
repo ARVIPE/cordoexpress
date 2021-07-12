41936128394a9c4f91c8e60e01761129c360adf7
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-07-12 19:52:42 +0200

    more effects and responsive added

--- includes/principal.php
+++ includes/principal.php
@@ -6,7 +6,7 @@
 
     <div class="hero-slide">
       <img alt="Mars Image" class="img-responsive cover" src="https://i.ibb.co/BB7rtQm/1.jpg">
-      <div class="header-content text-white position-absolute slide-content col-lg-4">
+      <div class="header-content text-white position-absolute col-lg-4" data-aos="fade-in" data-aos-delay="100">
         <h1 class="mb-4">Bienvenido a <span class="d-block font-weight-bold">CordoExpress</span></h1>
         <a class="btn btn-primary btn-lg w-max mt-2" href="#" tabindex="0">Contacto</a>
       </div>
@@ -15,7 +15,7 @@
 
     <div class="hero-slide">
       <img alt="Mars Image" class="img-responsive cover" src="https://i.ibb.co/2WtdR74/2.jpg">
-      <div class="header-content text-white position-absolute slide-content col-lg-4">
+      <div class="header-content text-white position-absolute slide col-lg-4" data-aos="fade-right" data-aos-delay="100">
         <h1 class="mb-4">CordoExpress</h1>
         <p class="font-weight-bold">Confíe su mercancía en un operador de transporte de amplia experiencia</p>
         <a class="btn btn-primary btn-lg w-max mt-2" href="#" tabindex="0">Contacto</a>
@@ -25,7 +25,7 @@
 
     <div class="hero-slide">
       <img alt="Mars Image" class="img-responsive cover" src="https://i.ibb.co/ys8TG5S/3.jpg">
-      <div class="header-content text-white position-absolute slide-content col-lg-4">
+      <div class="header-content text-white position-absolute slide col-lg-4" data-aos="fade-right" data-aos-delay="100">
       <h1 class="mb-4">Transportes</h1>
         <p>Transporte de por carretera, fundamentalmente desarrollando logística, transporte y distribución en la provincia de Córdoba. </p>
         <a class="btn btn-primary btn-lg w-max mt-2" href="#" tabindex="0">Contacto</a>
@@ -39,32 +39,30 @@
 
 <!-- === Start description === --->
 <div class="container" style="margin-top: 50px; margin-bottom: 30px;">
-
-
-<div class="row">
-  <div class="col" style="margin-top: 30px">
-  <h3><span class="tituloDescripcion">CordoExpress el mejor servicio posible.</span></h3>
-  <p>Si quiere más información sobre nuestros servicios puede llamarnos o utilizar la página de <a href="contacto.php">contacto</a> que aparece en la web. </br> Servicios: Transporte de carga paletizada fraccionada, completa, grupaje, cualquier servicio que nos demande en transporte nacional (doméstico) y Europeo.</p>
+  <div class="row">
+    <div class="col-md-6 col-lg-6 col-xl-6 col-sm-0" style="margin-top: 30px">
+    <h3><span class="tituloDescripcion">CordoExpress el mejor servicio posible.</span></h3>
+    <p>Si quiere más información sobre nuestros servicios puede llamarnos o utilizar la página de <a href="contacto.php">contacto</a> que aparece en la web. </br> Servicios: Transporte de carga paletizada fraccionada, completa, grupaje, cualquier servicio que nos demande en transporte nacional (doméstico) y Europeo.</p>
+    </div>
+    <div class="col-md-6 col-lg-6 col-xl-6 col-sm-0" data-aos="fade-right">
+     
+         <img data-aos="fade-right" data-aos-delay="100" src="https://i.ibb.co/4dRZ9Yy/descripcion1.jpg"  class="imagenDescripcion1">
+     
+    </div>
   </div>
-  <div class="col" data-aos="fade-right">
-  <img data-aos="fade-right" data-aos-delay="100" src="https://i.ibb.co/4dRZ9Yy/descripcion1.jpg"  class="imagenDescripcion">
+  <hr style="height:2px;border:none;color:#7c7c7c;;background-color:#7c7c7c;;"  />
+  <div class="row">
+    <div class="col">
+      <h4 class="tituloDescripcion" style="text-align: center;" >Servicios Palletways</h4>
+      <img  data-aos="fade-left" data-aos-delay="100" src="https://i.ibb.co/J2zqtYJ/descripcion2.png" class="imagenDescripcion">
+      <p>Servicios de transporte paletizado en 24 y 48 horas en la Peninsula Ibérica y servicios paneuropeos a partir de 36 horas.</p>
+    </div>
+    <div class="col">
+      <h4 class="tituloDescripcion" style="text-align: center;">Operador Logistico</h4>
+      <img data-aos="fade-right" data-aos-delay="100"  src="https://i.ibb.co/VJj6t29/descripcion3.jpg" class="imagenDescripcion">
+      <p>¿Necesitas almacenamiento? ¿Tienes cargas fraccionadas? ¿Cargas completas? Pidenos cotización sin compromiso. Ofrecemos los mejores servicios.</p>
+    </div>
   </div>
-</div>
-<hr style="height:2px;border:none;color:#7c7c7c;;background-color:#7c7c7c;;" />
-<div class="row">
-  <div class="col">
-    <h4 class="tituloDescripcion" style="text-align: center;" >Servicios Palletways</h4>
-    <img  data-aos="fade-left" data-aos-delay="100" src="https://i.ibb.co/J2zqtYJ/descripcion2.png" class="imagenDescripcion">
-    <p>Servicios de transporte paletizado en 24 y 48 horas en la Peninsula Ibérica y servicios paneuropeos a partir de 36 horas.</p>
-  </div>
-  <div class="col">
-    <h4 class="tituloDescripcion" style="text-align: center;">Operador Logistico</h4>
-    <img data-aos="fade-right" data-aos-delay="100"  src="https://i.ibb.co/VJj6t29/descripcion3.jpg" class="imagenDescripcion">
-    <p>¿Necesitas almacenamiento? ¿Tienes cargas fraccionadas? ¿Cargas completas? Pidenos cotización sin compromiso. Ofrecemos los </br>mejores servicios.</p>
-  </div>
-</div>
-
-
 </div>
 
 
