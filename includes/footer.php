cda57176e9cea80ff0eb64218e58caf7ae270eff
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-07-13 19:51:38 +0200

    footer done

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12 footer-contact">
+                    <p style="margin-top: 18px">
+                        <img class="logo" src="https://i.ibb.co/19K9mHV/logo-Principal.png">
+                    <div style="display:block; text-align:center;"><strong>Phone:</strong> +34 957 42 11 11<br></div>
+                    <div style="display:block; text-align:center;"><strong>Email:</strong> contabilidad@cordoexpress.es<br></div>
+                    </p>
+                </div>
+
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6 footer-links">
+                    <h4>Enlaces relevantes</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Nosotros</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Servicios</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Politica de privacidad</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs-6 footer-links">
+                    <h4>Nuestros servicios</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Transporte de carga</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Almacenamiento</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Cargas Fraccionadas</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Grupaje</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
