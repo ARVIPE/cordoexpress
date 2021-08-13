5ba8adb94308c6590bc63653146c47d1e16f5047
ARVIPE <55533930+ARVIPE@users.noreply.github.com>
2021-08-13 19:57:06 +0200

    Update emailTest.php

--- emailTest.php
+++ emailTest.php
@@ -2,8 +2,8 @@
 $sender = 'willyrex@gmail.com';
 $recipient = 'artfdl963@gmail.com';
 
-$subject = "php mail test";
-$message = "php test message";
+$subject = "Fiesta en la mansion youtuber";
+$message = "Te invito a mi casa con vergota si te sientas te la mete";
 $headers = 'From:' . $sender;
 
 if (mail($recipient, $subject, $message, $headers))
